6cfa90a39af01b2bb95828ba37a6aa9ee0b4e393
Almásy Gergő <almgergo@gmail.com>
2017-06-08 22:59:13 +0200

    MyFirstCommit

---
+++ build.sh
@@ -0,0 +1,2 @@
+clang++-3.9 main.cpp -I ../sycl_repository/triSYCL/include/ -std=c++1y
+./a.out
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <CL/sycl.hpp>
+#include <iostream>
+
+int main(){
+	std::cout << "HELLOWORLD" << std::endl;
+}
